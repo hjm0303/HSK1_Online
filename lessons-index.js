f511b36ab6c51bcd5c3bc9013edbcd403f33b774
minh <hejunming0303@gmail.com>
2026-07-25 08:17:27 +0700

    Add files via upload

--- lessons-index.js
+++ lessons-index.js
@@ -13,6 +13,24 @@ const LESSONS_INDEX = [
     pinyin: "Xièxie nǐ", 
     file: "lesson2.html", 
     level: "HSK 1",
+    locked: false,
+    password: "8888"
+  },
+    { 
+    id: 3, 
+    title: "你叫什么名字", 
+    pinyin: "Nǐ jiào shénme míngzi", 
+    file: "lesson3.html", 
+    level: "HSK 1",
+    locked: false,
+    password: "8888"
+  },
+    { 
+    id: 4, 
+    title: "她是我的汉语老师", 
+    pinyin: "Tā shì wǒ de hànyǔ lǎoshī", 
+    file: "lesson4.html", 
+    level: "HSK 1",
     locked: true,
     password: "8888"
   }
