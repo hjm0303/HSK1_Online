930e8d7ea21942bbb71c5369b5486943c2755a9e
minh <hejunming0303@gmail.com>
2026-07-25 13:26:33 +0700

    Add files via upload

--- lessons-index.js
+++ lessons-index.js
@@ -13,25 +13,110 @@ const LESSONS_INDEX = [
     pinyin: "Xièxie nǐ", 
     file: "lesson2.html", 
     level: "HSK 1",
-    locked: false,
-    password: "8888"
+    locked: false
   },
-    { 
+  { 
     id: 3, 
     title: "你叫什么名字", 
     pinyin: "Nǐ jiào shénme míngzi", 
     file: "lesson3.html", 
     level: "HSK 1",
-    locked: false,
-    password: "8888"
+    locked: false
   },
-    { 
+  { 
     id: 4, 
     title: "她是我的汉语老师", 
     pinyin: "Tā shì wǒ de hànyǔ lǎoshī", 
     file: "lesson4.html", 
     level: "HSK 1",
-    locked: true,
-    password: "8888"
+    locked: false
+  },
+  { 
+    id: 5, 
+    title: "她女儿今年二十岁", 
+    pinyin: "Tā nǚ'ér jīnnián èrshí suì", 
+    file: "lesson5.html", 
+    level: "HSK 1",
+    locked: false
+  },
+  { 
+    id: 6, 
+    title: "我会说汉语", 
+    pinyin: "Wǒ huì shuō Hànyǔ", 
+    file: "lesson6.html", 
+    level: "HSK 1",
+    locked: false
+  },
+  { 
+    id: 7, 
+    title: "今天几号", 
+    pinyin: "Jīntiān jǐ hào", 
+    file: "lesson7.html", 
+    level: "HSK 1",
+    locked: false
+  },
+  { 
+    id: 8, 
+    title: "我想喝茶", 
+    pinyin: "Wǒ xiǎng hē chá", 
+    file: "lesson8.html", 
+    level: "HSK 1",
+    locked: false
+  },
+  { 
+    id: 9, 
+    title: "你儿子在哪里工作", 
+    pinyin: "Nǐ érzi zài nǎlǐ gōngzuò", 
+    file: "lesson9.html", 
+    level: "HSK 1",
+    locked: false
+  },
+  { 
+    id: 10, 
+    title: "我能坐这儿吗", 
+    pinyin: "Wǒ néng zuò zhèr ma", 
+    file: "lesson10.html", 
+    level: "HSK 1",
+    locked: false
+  },
+  { 
+    id: 11, 
+    title: "现在几点", 
+    pinyin: "Xiànzài jǐ diǎn", 
+    file: "lesson11.html", 
+    level: "HSK 1",
+    locked: false
+  },
+  { 
+    id: 12, 
+    title: "明天天气怎么样", 
+    pinyin: "Míngtiān tiānqì zěnmeyàng", 
+    file: "lesson12.html", 
+    level: "HSK 1",
+    locked: false
+  },
+  { 
+    id: 13, 
+    title: "他在学做中国菜呢", 
+    pinyin: "Tā zài xué zuò Zhōngguó cài ne", 
+    file: "lesson13.html", 
+    level: "HSK 1",
+    locked: false
+  },
+  { 
+    id: 14, 
+    title: "她买了不少衣服", 
+    pinyin: "Tā mǎi le bù shǎo yīfu", 
+    file: "lesson14.html", 
+    level: "HSK 1",
+    locked: false
+  },
+  { 
+    id: 15, 
+    title: "我是坐飞机来的", 
+    pinyin: "Wǒ shì zuò fēijī lái de", 
+    file: "lesson15.html", 
+    level: "HSK 1",
+    locked: false
   }
 ];
